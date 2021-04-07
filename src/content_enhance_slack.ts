ccbd2b5b703e50db8b9ce6403f9e6f57fc8e2b12
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2021-04-07 12:49:40 +1000

    Rename source files

---
+++ src/content_enhance_slack.ts
@@ -0,0 +1,5 @@
+
+// todo: hide this if it contains "desktop notifications"
+//  document.querySelector('.p-message_pane__foreword__notification_warning');
+
+// todo: hide coloured thumbs
--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -2,21 +2,33 @@ const webpack = require("webpack");
 const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
 const srcDir = path.join(__dirname, "..", "src");
+const _ = require("lodash");
+
+function makeEntries(entries) {
+  return _(entries)
+    .flatMap((names, ext) => _(names)
+      .map(name => [name, path.join(srcDir, `${name}.${ext}`)])
+      .value())
+    .fromPairs()
+    .value();
+}
 
 module.exports = {
-    entry: {
-      grabToken: path.join(srcDir, 'grabToken.ts'),
-      orgCaptureSlack: path.join(srcDir, 'orgCaptureSlack.ts'),
-      content_script: path.join(srcDir, 'content_script.ts'),
-      slackinbrowser: path.join(srcDir, 'slackinbrowser.ts'),
-      popup: path.join(srcDir, 'popup.ts'),
-      options: path.join(srcDir, 'options.ts'),
+    entry: makeEntries({
+      ts: [
+        "background_grab_token",
+        "background_org_capture_slack",
+        "content_capture_button",
+        "content_enhance_slack",
+        "content_slack_in_browser",
+        "popup",
+        "options",
+      ],
       // these could be built with react
-      // popup: path.join(srcDir, 'popup.tsx'),
-      // options: path.join(srcDir, 'options.tsx'),
-    },
+      // tsx: [ "popup", "options" ]
+    }),
     output: {
-        path: path.join(__dirname, "../dist/js"),
+        path: path.join(__dirname, "../dist"),
         filename: "[name].js",
     },
     optimization: {
