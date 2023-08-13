fc704362185c27d7a4611d905cc341d7f1559ffa
R Primus <rprimus@gmail.com>
2023-08-13 10:31:04 +0100

    Add semi-colon

--- src/content_capture_button.ts
+++ src/content_capture_button.ts
@@ -40,7 +40,7 @@ function isOurButton(el: HTMLButtonElement) {
 document.addEventListener("click", e => {
   for (let target = e.target; target && target !== document; target = (target as Element).parentNode) {
     // console.log("slack body clicked - target", target);
-    const button = target as HTMLButtonElement
+    const button = target as HTMLButtonElement;
     if (isOurButton(button)) {
       console.log("it's ours, with dataset", button.dataset);
       const meta = button.dataset.focusMetadata ? JSON.parse(button.dataset.focusMetadata) : undefined;
